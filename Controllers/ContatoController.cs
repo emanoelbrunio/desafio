e2c2ffddf481d0b3db3264d83c9e8bdc07eae21f
Emanoel Brunio <emanoelbrunio@gmail.com>
2023-01-17 10:13:06 -0300

    first commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleBNE.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Criar()
+        {
+
+            return View();
+        }
+        public IActionResult Editar()
+        {
+
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleBNE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
